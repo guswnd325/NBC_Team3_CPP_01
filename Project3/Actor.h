4d4cf308d8251320d3c19f96d175645ceca5072b
HYEONJOONG\gksrp <guswnd325.code@gmail.com>
2026-03-27 15:33:13 +0900

    [현중][프레임워크] 기본 프레임워크 구조 파일 추가

---
+++ Project3/Actor.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <string>
+
+constexpr int MAX_HP = 100;
+
+class Actor
+{
+private:
+	std::string name;
+	int hp, gold, atk, def, exp;
+public:
+	std::string GetName() { return name; }
+	
+	bool IsDead() { return hp <= 0; }
+
+	void SetHP(int value) { hp = value; }
+	int GetHP() { return hp; }
+	
+	void SetGold(int value) { gold = value; }
+	int GetGold() { return gold; }
+
+	void SetAtk(int value) { atk = value; }
+	int GetAtk() { return atk; }
+
+	void SetDef(int value) { def = value; }
+	int GetDef() { return def; }
+
+	void SetExp(int value) { exp = value; }
+	int GetExp() { return exp; }
+};
---
+++ Project3/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	cout << "Hello, New team 3" << endl;
+	return 0;
+}
