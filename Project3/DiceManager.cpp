2bcbdae2f98e234a1473f9277b43649024e8fc1f
blurqls <blurwuri@gmail.com>
2026-03-30 21:05:19 +0900

    [김준수][다이스 매니저] 주사위 최저값 로직 추가 밑 애니메이션 변경

--- Project3/DiceManager.cpp
+++ Project3/DiceManager.cpp
@@ -11,6 +11,102 @@ std::mt19937 DiceManager::gen(DiceManager::rd());
 
 DiceManager::DiceManager() {}
 DiceManager::~DiceManager() {}
+
+
+void DiceManager::DiceAnimationRollNumber(const std::vector<int>& results)
+{
+    HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+    DWORD dwMode = 0;
+    GetConsoleMode(hOut, &dwMode);
+    SetConsoleMode(hOut, dwMode | ENABLE_VIRTUAL_TERMINAL_PROCESSING);
+
+    const int FRAME_LINES = 5;
+    int diceCount = (int)results.size();
+
+    CONSOLE_CURSOR_INFO cursorInfo;
+    GetConsoleCursorInfo(hOut, &cursorInfo);
+    cursorInfo.bVisible = FALSE;
+    SetConsoleCursorInfo(hOut, &cursorInfo);
+
+    srand(static_cast<unsigned int>(time(nullptr)));
+    AudioManager::GetInstance().PlaySFX(SFXList::dice_roll);
+
+    
+    auto makeFrame = [](int num) -> std::vector<std::string> {
+        std::string numStr = std::to_string(num);
+        
+        std::string center;
+        if (numStr.size() == 1)       center = "  " + numStr + "  ";
+        else if (numStr.size() == 2)  center = "  " + numStr + " ";
+        else                          center = " " + numStr + " ";
+
+        return {
+            "+-------+",
+            "|       |",
+            "| " + center + " |",
+            "|       |",
+            "+-------+"
+        };
+        };
+
+    
+    auto firstFrame = makeFrame(rand() % 9 + 1);
+    for (int line = 0; line < FRAME_LINES; ++line) {
+        std::cout << "\t";
+        for (int d = 0; d < diceCount; ++d) {
+            std::cout << firstFrame[line] << "  ";
+        }
+        std::cout << "\n";
+    }
+
+    
+    for (int i = 1; i < 16; ++i) {
+        std::cout << "\033[" << FRAME_LINES << "A";
+
+        std::vector<std::vector<std::string>> frameIdxs(diceCount);
+        for (int d = 0; d < diceCount; ++d) {
+            frameIdxs[d] = makeFrame(rand() % 9 + 1);
+        }
+
+        for (int line = 0; line < FRAME_LINES; ++line) {
+            std::cout << "\r\t";
+            for (int d = 0; d < diceCount; ++d) {
+                std::cout << frameIdxs[d][line] << "  ";
+            }
+            std::cout << "\n";
+        }
+        std::cout.flush();
+
+        int delay = 10 + (i * i);
+        std::this_thread::sleep_for(std::chrono::milliseconds(delay));
+    }
+
+    
+    std::cout << "\033[" << FRAME_LINES << "A";
+    for (int line = 0; line < FRAME_LINES; ++line) {
+        std::cout << "\r\t";
+        for (int d = 0; d < diceCount; ++d) {
+            auto resultFrame = makeFrame(results[d]);
+            std::cout << resultFrame[line] << "  ";
+        }
+        std::cout << "\n";
+    }
+    std::cout.flush();
+
+    cursorInfo.bVisible = TRUE;
+    SetConsoleCursorInfo(hOut, &cursorInfo);
+
+    
+    std::cout << "\n\t";
+    for (int d = 0; d < diceCount; ++d) {
+        std::cout << "[  " << results[d] << "  ]" << "  ";
+    }
+
+    int total = 0;
+    for (int r : results) total += r;
+    std::cout << std::endl;
+}
+
 void DiceManager::DiceAnimationRoll(const std::vector<int>& results)
 {
     HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
@@ -118,7 +214,7 @@ int DiceManager::Roll(Character* character)
         }
     }
 
-    DiceAnimationRoll(results);
+    DiceAnimationRollNumber({totalSum});
 
     return totalSum;
 }
@@ -129,137 +225,16 @@ void DiceManager::SetDiceID(Dice& dice)
     int min = dice.minSide;
     int max = dice.maxSide;
 
-    if (min == 1) {
-        if (max == 2)  dice.id = DiceID::D1_2;
-        else if (max == 3)  dice.id = DiceID::D1_3;
-        else if (max == 4)  dice.id = DiceID::D1_4;
-        else if (max == 5)  dice.id = DiceID::D1_5;
-        else if (max == 6)  dice.id = DiceID::D1_6;
-        else if (max == 7)  dice.id = DiceID::D1_7;
-        else if (max == 8)  dice.id = DiceID::D1_8;
-        else if (max == 9)  dice.id = DiceID::D1_9;
-        else if (max == 10) dice.id = DiceID::D1_10;
-        else if (max == 11) dice.id = DiceID::D1_11;
-        else if (max == 12) dice.id = DiceID::D1_12;
-        else if (max == 13) dice.id = DiceID::D1_13;
-        else if (max == 14) dice.id = DiceID::D1_14;
-        else if (max == 15) dice.id = DiceID::D1_15;
-        else if (max == 16) dice.id = DiceID::D1_16;
-        else if (max == 17) dice.id = DiceID::D1_17;
-        else if (max == 18) dice.id = DiceID::D1_18;
-        else if (max == 19) dice.id = DiceID::D1_19;
-        else if (max == 20) dice.id = DiceID::D1_20;
-        else if (max == 21) dice.id = DiceID::D1_21;
-        else if (max == 22) dice.id = DiceID::D1_22;
-        else if (max == 23) dice.id = DiceID::D1_23;
-        else if (max == 24) dice.id = DiceID::D1_24;
-        else if (max == 25) dice.id = DiceID::D1_25;
-        else if (max == 26) dice.id = DiceID::D1_26;
-        else if (max == 27) dice.id = DiceID::D1_27;
+    int offset = 0;
+    int count = 27;  // 26 ¡æ 27
+    for (int m = 1; m < min; m++) {
+        offset += count;
+        count--;
     }
-    else if (min == 2) {
-        if (max == 3)       dice.id = DiceID::D2_3;
-        else if (max == 4)  dice.id = DiceID::D2_4;
-        else if (max == 5)  dice.id = DiceID::D2_5;
-        else if (max == 6)  dice.id = DiceID::D2_6;
-        else if (max == 7)  dice.id = DiceID::D2_7;
-        else if (max == 8)  dice.id = DiceID::D2_8;
-        else if (max == 9)  dice.id = DiceID::D2_9;
-        else if (max == 10) dice.id = DiceID::D2_10;
-        else if (max == 11) dice.id = DiceID::D2_11;
-        else if (max == 12) dice.id = DiceID::D2_12;
-        else if (max == 13) dice.id = DiceID::D2_13;
-        else if (max == 14) dice.id = DiceID::D2_14;
-        else if (max == 15) dice.id = DiceID::D2_15;
-        else if (max == 16) dice.id = DiceID::D2_16;
-        else if (max == 17) dice.id = DiceID::D2_17;
-        else if (max == 18) dice.id = DiceID::D2_18;
-        else if (max == 19) dice.id = DiceID::D2_19;
-        else if (max == 20) dice.id = DiceID::D2_20;
-        else if (max == 21) dice.id = DiceID::D2_21;
-        else if (max == 22) dice.id = DiceID::D2_22;
-        else if (max == 23) dice.id = DiceID::D2_23;
-        else if (max == 24) dice.id = DiceID::D2_24;
-        else if (max == 25) dice.id = DiceID::D2_25;
-        else if (max == 26) dice.id = DiceID::D2_26;
-        else if (max == 27) dice.id = DiceID::D2_27;
-    }
-    else if (min == 3) {
-        if (max == 4)       dice.id = DiceID::D3_4;
-        else if (max == 5)  dice.id = DiceID::D3_5;
-        else if (max == 6)  dice.id = DiceID::D3_6;
-        else if (max == 7)  dice.id = DiceID::D3_7;
-        else if (max == 8)  dice.id = DiceID::D3_8;
-        else if (max == 9)  dice.id = DiceID::D3_9;
-        else if (max == 10) dice.id = DiceID::D3_10;
-        else if (max == 11) dice.id = DiceID::D3_11;
-        else if (max == 12) dice.id = DiceID::D3_12;
-        else if (max == 13) dice.id = DiceID::D3_13;
-        else if (max == 14) dice.id = DiceID::D3_14;
-        else if (max == 15) dice.id = DiceID::D3_15;
-        else if (max == 16) dice.id = DiceID::D3_16;
-        else if (max == 17) dice.id = DiceID::D3_17;
-        else if (max == 18) dice.id = DiceID::D3_18;
-        else if (max == 19) dice.id = DiceID::D3_19;
-        else if (max == 20) dice.id = DiceID::D3_20;
-        else if (max == 21) dice.id = DiceID::D3_21;
-        else if (max == 22) dice.id = DiceID::D3_22;
-        else if (max == 23) dice.id = DiceID::D3_23;
-        else if (max == 24) dice.id = DiceID::D3_24;
-        else if (max == 25) dice.id = DiceID::D3_25;
-        else if (max == 26) dice.id = DiceID::D3_26;
-        else if (max == 27) dice.id = DiceID::D3_27;
-    }
-    else if (min == 4) {
-        if (max == 5)       dice.id = DiceID::D4_5;
-        else if (max == 6)  dice.id = DiceID::D4_6;
-        else if (max == 7)  dice.id = DiceID::D4_7;
-        else if (max == 8)  dice.id = DiceID::D4_8;
-        else if (max == 9)  dice.id = DiceID::D4_9;
-        else if (max == 10) dice.id = DiceID::D4_10;
-        else if (max == 11) dice.id = DiceID::D4_11;
-        else if (max == 12) dice.id = DiceID::D4_12;
-        else if (max == 13) dice.id = DiceID::D4_13;
-        else if (max == 14) dice.id = DiceID::D4_14;
-        else if (max == 15) dice.id = DiceID::D4_15;
-        else if (max == 16) dice.id = DiceID::D4_16;
-        else if (max == 17) dice.id = DiceID::D4_17;
-        else if (max == 18) dice.id = DiceID::D4_18;
-        else if (max == 19) dice.id = DiceID::D4_19;
-        else if (max == 20) dice.id = DiceID::D4_20;
-        else if (max == 21) dice.id = DiceID::D4_21;
-        else if (max == 22) dice.id = DiceID::D4_22;
-        else if (max == 23) dice.id = DiceID::D4_23;
-        else if (max == 24) dice.id = DiceID::D4_24;
-        else if (max == 25) dice.id = DiceID::D4_25;
-        else if (max == 26) dice.id = DiceID::D4_26;
-        else if (max == 27) dice.id = DiceID::D4_27;
-    }
-    else if (min == 5) {
-        if (max == 6)       dice.id = DiceID::D5_6;
-        else if (max == 7)  dice.id = DiceID::D5_7;
-        else if (max == 8)  dice.id = DiceID::D5_8;
-        else if (max == 9)  dice.id = DiceID::D5_9;
-        else if (max == 10) dice.id = DiceID::D5_10;
-        else if (max == 11) dice.id = DiceID::D5_11;
-        else if (max == 12) dice.id = DiceID::D5_12;
-        else if (max == 13) dice.id = DiceID::D5_13;
-        else if (max == 14) dice.id = DiceID::D5_14;
-        else if (max == 15) dice.id = DiceID::D5_15;
-        else if (max == 16) dice.id = DiceID::D5_16;
-        else if (max == 17) dice.id = DiceID::D5_17;
-        else if (max == 18) dice.id = DiceID::D5_18;
-        else if (max == 19) dice.id = DiceID::D5_19;
-        else if (max == 20) dice.id = DiceID::D5_20;
-        else if (max == 21) dice.id = DiceID::D5_21;
-        else if (max == 22) dice.id = DiceID::D5_22;
-        else if (max == 23) dice.id = DiceID::D5_23;
-        else if (max == 24) dice.id = DiceID::D5_24;
-        else if (max == 25) dice.id = DiceID::D5_25;
-        else if (max == 26) dice.id = DiceID::D5_26;
-        else if (max == 27) dice.id = DiceID::D5_27;
-    }
+    dice.id = static_cast<DiceID>(offset + (max - min)); 
 }
+
+
 void DiceManager::UpdateMin(Character* character, int value) {
     if (!character || !character->GetInventory()) return;
 
@@ -351,10 +326,11 @@ void DiceManager::UpdateMax(Character* character, int value) {
 //
 DiceID DiceManager::MakeDiceId(int min, int max)
 {
-    Dice temp(DiceID::D1_2);
-    temp.minSide = min;
-    temp.maxSide = max;
-    SetDiceID(temp);
-    return temp.id;
+    int offset = 0;
+    int count = 27;  // 26 ¡æ 27
+    for (int m = 1; m < min; m++) {
+        offset += count;
+        count--;
+    }
+    return static_cast<DiceID>(offset + (max - min)); 
 }
-
--- Project3/DiceManager.h
+++ Project3/DiceManager.h
@@ -13,37 +13,33 @@ class AudioManager;
 
 enum class DiceID
 {
-    // min = 1
-    D1_2, D1_3, D1_4, D1_5, D1_6,
+    // min = 1 (27개: D1_1 ~ D1_27)
+    D1_1, D1_2, D1_3, D1_4, D1_5, D1_6,
     D1_7, D1_8, D1_9, D1_10, D1_11,
     D1_12, D1_13, D1_14, D1_15, D1_16,
     D1_17, D1_18, D1_19, D1_20, D1_21,
     D1_22, D1_23, D1_24, D1_25, D1_26,
     D1_27,
-
-    // min = 2
-    D2_3, D2_4, D2_5, D2_6, D2_7,
+    // min = 2 (26개: D2_2 ~ D2_27)
+    D2_2, D2_3, D2_4, D2_5, D2_6, D2_7,
     D2_8, D2_9, D2_10, D2_11, D2_12,
     D2_13, D2_14, D2_15, D2_16, D2_17,
     D2_18, D2_19, D2_20, D2_21, D2_22,
     D2_23, D2_24, D2_25, D2_26, D2_27,
-
-    // min = 3
-    D3_4, D3_5, D3_6, D3_7, D3_8,
+    // min = 3 (25개: D3_3 ~ D3_27)
+    D3_3, D3_4, D3_5, D3_6, D3_7, D3_8,
     D3_9, D3_10, D3_11, D3_12, D3_13,
     D3_14, D3_15, D3_16, D3_17, D3_18,
     D3_19, D3_20, D3_21, D3_22, D3_23,
     D3_24, D3_25, D3_26, D3_27,
-
-    // min = 4
-    D4_5, D4_6, D4_7, D4_8, D4_9,
+    // min = 4 (24개: D4_4 ~ D4_27)
+    D4_4, D4_5, D4_6, D4_7, D4_8, D4_9,
     D4_10, D4_11, D4_12, D4_13, D4_14,
     D4_15, D4_16, D4_17, D4_18, D4_19,
     D4_20, D4_21, D4_22, D4_23, D4_24,
     D4_25, D4_26, D4_27,
-
-    // min = 5
-    D5_6, D5_7, D5_8, D5_9, D5_10,
+    // min = 5 (23개: D5_5 ~ D5_27)
+    D5_5, D5_6, D5_7, D5_8, D5_9, D5_10,
     D5_11, D5_12, D5_13, D5_14, D5_15,
     D5_16, D5_17, D5_18, D5_19, D5_20,
     D5_21, D5_22, D5_23, D5_24, D5_25,
@@ -80,16 +76,15 @@ struct Dice
         id = diceId;
         int idx = static_cast<int>(diceId);
 
-        // min=1: 26개, min=2: 25개, min=3: 24개, min=4: 23개, min=5: 22개
         int min = 1;
-        int count = 26;
+        int count = 27;  // 26 → 27
         while (idx >= count) {
             idx -= count;
             min++;
             count--;
         }
         minSide = min;
-        maxSide = min + 1 + idx;
+        maxSide = min + idx;  // min + 1 + idx → min + idx
     }
 };
 
@@ -119,6 +114,8 @@ public:
 
 
     void DiceAnimationRoll(const std::vector<int>& results);
+
+    void DiceAnimationRollNumber(const std::vector<int>& results);
 
 };
 
